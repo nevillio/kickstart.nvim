9b84852db048a4fdb5ced2d7419245f9a7e1d8ae
nevillio <nevilabrahamelias@gmail.com>
2024-09-19 11:22:40 +0530

    Add Treesitter Context

--- init.lua
+++ init.lua
@@ -421,6 +421,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>tc', '<cmd>TSContext toggle<cr>', { desc = '[T]oggle [C]ontext' })
 
       -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
       -- it is better explained there). This allows easily switching between pickers if you prefer using something else!
@@ -996,6 +997,7 @@ require('lazy').setup({
       })
     end,
   },
+  'nvim-treesitter/nvim-treesitter-context',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
