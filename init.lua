5d1b1cf2be45c9fe49a393e43653885a68731ad8
nevillio <nevilabrahamelias@gmail.com>
2024-09-19 15:30:42 +0530

    Add Telescope pickers and Key Bindings
    Add pickers for notes and configs
    Modify layout and other tweaks

--- init.lua
+++ init.lua
@@ -451,12 +451,23 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          layout_config = {
+            horizontal = {
+              preview_cutoff = 90,
+              preview_width = 0.45,
+              width = 0.85,
+            },
+          },
+          path_display = { 'smart' },
+          mappings = {
+          },
+          cache_picker = {
+            num_pickers = 3,
+            limit_entries = 100,
+          },
+        },
+        pickers = {},
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -478,6 +489,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sp', builtin.pickers, { desc = '[S]earch [P]ickers' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope<cr>', { desc = '[S]earch [T]odos' })
@@ -507,7 +519,29 @@ require('lazy').setup({
       )
 
       -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
+      vim.keymap.set('n', '<leader>sc', function()
+        local dirnames = { 'nvim', 'kitty', 'hypr', 'wofi', 'waybar', 'mpv', 'qutebrowser', 'lazygit', 'onemw', 'aichat' }
+        local path = '~/.config/'
+        local dirs = {}
+        for _, dirname in ipairs(dirnames) do
+          table.insert(dirs, path .. dirname)
+        end
+        table.insert(dirs, '~/crucible')
+        builtin.find_files {
+          prompt_title = 'Search config files',
+          results_title = 'Config Files',
+          search_dirs = dirs,
+        }
+      end, { desc = '[S]earch [C]onfig files' })
+      vim.keymap.set('n', '<leader>sn', function()
+        local notes_directories = {
+          '~/Documents/notes',
+        }
+        builtin.live_grep {
+          prompt_title = 'Grep Notes',
+          search_dirs = notes_directories,
+        }
+      end, { desc = '[S]earch [N]otes' })
     end,
   },
 
