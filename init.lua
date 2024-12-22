53cf8c72dae52898340618f779eace9bb915fc81
nevillio <nevilabrahamelias@gmail.com>
2024-12-22 20:11:12 +0530

    Add new theme
    Add winter is coming
    Change to tokyonight-moon

--- init.lua
+++ init.lua
@@ -975,9 +975,10 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
+  'atmosuwiryo/vim-winteriscoming',
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
