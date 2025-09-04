e9896e2e640770ef6173fe808d6a95ff9b891266
nevillio <nevilabrahamelias@gmail.com>
2025-09-04 23:37:07 +0530

    Add nvim-dap-highlights and nvim-dap-virtual-text
    Add nvim-dap-highlights and nvim-dap-virtual-text

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -15,9 +15,24 @@ return {
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
+    'theHamsta/nvim-dap-virtual-text',
 
     -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
+
+    {
+      'LiadOz/nvim-dap-repl-highlights',
+      config = true,
+      dependencies = {
+        'mfussenegger/nvim-dap',
+        'nvim-treesitter/nvim-treesitter',
+      },
+      build = function()
+        if not require('nvim-treesitter.parsers').has_parser 'dap_repl' then
+          vim.cmd ':TSInstall dap_repl'
+        end
+      end,
+    },
 
     -- Installs the debug adapters for you
     'mason-org/mason.nvim',
@@ -105,6 +120,7 @@ return {
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
+    local present_virtual_text, dap_vt = pcall(require, 'nvim-dap-virtual-text')
 
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
@@ -122,6 +138,26 @@ return {
         'delve',
         'js-debug-adapter',
       },
+    }
+
+    -- ╭──────────────────────────────────────────────────────────╮
+    -- │ DAP Virtual Text Setup                                   │
+    -- ╰──────────────────────────────────────────────────────────╯
+    dap_vt.setup {
+      enabled = true, -- enable this plugin (the default)
+      enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+      highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+      highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+      show_stop_reason = true, -- show stop reason when stopped for exceptions
+      commented = false, -- prefix virtual text with comment string
+      only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+      all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+      filter_references_pattern = '<module', -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
+      -- Experimental Features:
+      virt_text_pos = 'eol', -- position of virtual text, see `:h nvim_buf_set_extmark()`
+      all_frames = true, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+      virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+      virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
     }
 
     -- Dap UI setup
@@ -163,6 +199,9 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    --Enable virtual text
+    vim.g.dap_virtual_text = true
 
     -- Install golang specific config
     require('dap-go').setup {
