79d313f5269b3c42c2f830b560b29400aa950c63
nevillio <nevilabrahamelias@gmail.com>
2026-03-09 11:02:17 +0530

    Add rounded border and undofile

--- init.lua
+++ init.lua
@@ -100,6 +100,9 @@ vim.o.scrolloff = 10
 
 -- turn off swapfile
 vim.opt.swapfile = false
+vim.opt.undofile = true
+
+vim.o.winborder = 'rounded'
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
